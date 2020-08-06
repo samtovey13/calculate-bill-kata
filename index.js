bfed979ceae08dda762b3563b852bf47fe55f912
Sam Tovey <samtovey13@gmail.com>
2020-08-06 17:07:52 +0100

    backup all files

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-function calculateBill() {
+function calculateBill(amount, vat, tip) {
+    const errorMessage = "error"
+    if (typeof amount !== 'number') {
+        return errorMessage;
+    };
+    if (typeof vat !== 'number') {
+        return errorMessage;
+    };
+    if (typeof tip !== 'number') {
+        return errorMessage;
+    };
 
+    const total = amount + (amount * vat / 100) + (amount * tip / 100);
+
+    return "£" + total;
 };
 
 module.exports = calculateBill;
